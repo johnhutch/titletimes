80d436f14da947578366654c998bdd4dcd6a493c
Jonathan Hall <jonathan.edward.hall@gmail.com>
2013-09-13 15:38:46 -0400

    Views for devise items have been generated

--- app/controllers/titletimes_controller.rb
+++ app/controllers/titletimes_controller.rb
@@ -1,5 +1,5 @@
 class TitletimesController < ApplicationController
-  before_filter :authenticate_user!
+  before_filter :authenticate_user!, only: [:edit, :update, :destroy]
   before_action :set_titletime, only: [:show, :edit, :update, :destroy]
 
   # GET /titletimes
